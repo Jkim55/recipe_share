e3d2831da871d4a74167b4100afee88aaf1506e8
Ji Kim <jkim55@gmail.com>
2016-12-01 19:23:51 -0700

    view and etc

--- Gemfile
+++ Gemfile
@@ -13,11 +13,11 @@ gem 'jbuilder', '~> 2.5'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'simple_form', '~> 3.3', '>= 3.3.1'
+gem 'cocoon', '~> 1.2', '>= 1.2.9'
 gem 'devise', '~> 4.2'
 gem 'paperclip', '~> 5.1'
 gem 'masonry-rails', '~> 0.2.4'
 gem 'acts_as_votable', '~> 0.10.0'
-
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,18 @@ class Recipe < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { large: "750x750>", thumb: "200x200>"  }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_many :ingredients, dependent: :destroy
+  has_many :directions, dependent: :destroy
+
+  accepts_nested_attributes_for :ingredients,
+                              reject_if: proc { |attributes| attributes['name'].blank? },
+                              allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                              reject_if: proc { |attributes| attributes['step'].blank? },
+                              allow_destroy: true
 
   validates :title, :description, :image, presence: true
+
+  has_attached_file :image, styles: { large: "750x750>", thumb: "200x200>"  }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
