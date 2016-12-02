7ba2c999264c742f28597b9c187e7c366b9d0a19
Ji Kim <jkim55@gmail.com>
2016-12-01 18:15:49 -0700

    minor edits to css

--- app/assets/javascripts/recipes.coffee
+++ app/assets/javascripts/recipes.coffee
@@ -3,7 +3,7 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $ ->
-  $('#pins').imagesLoaded ->
-    $('#pins').masonry
+  $('#recipes').imagesLoaded ->
+    $('#recipes').masonry
       itemSelector: '.box'
       isFitWidth: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { large: "750x750>", thumb: "100x100>"  }
+  has_attached_file :image, styles: { large: "750x750>", thumb: "200x200>"  }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :title, :description, :image, presence: true
 end
