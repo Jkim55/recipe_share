9eb0ec4a3885d397e0cd61ea044d6a064843a098
Ji Kim <jkim55@gmail.com>
2016-12-04 00:59:09 -0700

    added meal/course model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,7 +47,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :image, category_ids: [] , ingredients_attributes: [:id, :name, :image, :_destroy], directions_attributes: [:id, :step, :attachment, :_destroy])
+    params.require(:recipe).permit(:title, :description, :image, category_ids: [] , course_ids: [], ingredients_attributes: [:id, :name, :image, :_destroy], directions_attributes: [:id, :step, :attachment, :_destroy])
   end
 
   def find_recipe
